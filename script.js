27a931111010940297cbfea421ae8a5d56697b9b
Daniel Xu <xudh47@gmail.com>
2022-08-11 22:26:22 -0700

    Made sure APIs are working

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-var weatherURL =
-  "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={54025b88abec8d86cdfc8b7b376ecf31}";
+var weatherURL = "";
+
+// "https://api.openweathermap.org/data/2.5/onecall?lat=35&lon=139&units=imperial&exclude=minutely,hourly&appid=54025b88abec8d86cdfc8b7b376ecf31";
+
+// Set a variable for search and all temperal literals
+// var weatherGeoURL = `api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+var search = "los angeles";
+var weatherGeoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=54025b88abec8d86cdfc8b7b376ecf31`;
 
 // var searchHistory = [];
 // var searchHistoryContainer = document.querySelector("#history");
 
-//API
-fetch(weatherURL)
+//API for weather
+//
+
+//API for Geo
+fetch(weatherGeoURL)
   .then(function (response) {
-    console.log(response);
+    // console.log(response);
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data[0].lon}&units=imperial&exclude=minutely,hourly&appid=54025b88abec8d86cdfc8b7b376ecf31`
+    )
+      .then(function (response) {
+        // console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
   });
 
 // function renderSearchHistory() {
